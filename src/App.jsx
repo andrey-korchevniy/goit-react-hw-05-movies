e42d9c6fd84cd91c0e98aa3ee92847dd341306d3
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-08-02 16:13:57 +0300

    11

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import { Layout } from "./Layout/Layout";
+import { Layout } from "./components/Layout/Layout";
 import { Home } from "pages/Home/Home";
 import { Movies } from "pages/Movies/Movies";
-import { MovieDetails } from "./MovieDetails/MovieDetails";
-import { Cast } from "./Cast/Cast";
-import { Reviews } from "./Reviews/Reviews";
+import { MovieDetails } from "./components/MovieDetails/MovieDetails";
+import { Cast } from "./components/Cast/Cast";
+import { Reviews } from "./components/Reviews/Reviews";
 
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout />}>                          
-          <Route path="/" element={<Home>dashboard</Home>} />
+          <Route path="/" element={<Home />} />
           <Route path='movies' element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path='cast' element={<Cast />} />
